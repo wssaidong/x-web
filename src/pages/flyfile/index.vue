7a791478c967394dfd109f47887a19fad4e67e7d
2449149803@qq.com <2449149803@qq.com>
2018-04-23 00:59:38 +0800

    commit

--- src/pages/flyfile/index.vue
+++ src/pages/flyfile/index.vue
@@ -6,48 +6,56 @@
 	}
 </style>
 <template>
-	<el-container>
 
-		<el-header title="flyfile">
+	<el-container >
 
+		<el-header>
 		</el-header>
-		<el-main>
-			<el-form ref="form" :model="form" label-width="80px">
-				<el-form-item>
-					<el-input
-						placeholder="文件码"
-						prefix-icon="el-icon-search"
-						v-model="fileCode">
-					</el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="onSubmit">寻找</el-button>
-					<el-button>取消</el-button>
-				</el-form-item>
-			</el-form>
-		</el-main>
+		<el-container direction="horizontal">
 
-		<el-aside width="40%">
-			<el-form ref="form" :model="form" label-width="80px">
-				<el-form-item>
-					<el-input
+			<el-main>
+				<el-form ref="form" :model="form" label-width="80px">
+					<el-form-item>
+						<el-input
 							placeholder="文件码"
 							prefix-icon="el-icon-search"
 							v-model="fileCode">
-					</el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="onSubmit">寻找</el-button>
-					<el-button>取消</el-button>
-				</el-form-item>
-			</el-form>
-		</el-aside>
+						</el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button type="primary" @click="onSubmit">寻找</el-button>
+						<el-button>取消</el-button>
+					</el-form-item>
+				</el-form>
+			</el-main>
+			<el-aside width="40%">
+				<el-main>
+					<el-form ref="form" :model="form" label-width="80px">
+						<el-form-item>
+							<el-input
+									placeholder="文件码"
+									prefix-icon="el-icon-search"
+									v-model="fileCode">
+							</el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-button type="primary" @click="onSubmit">寻找</el-button>
+							<el-button>取消</el-button>
+						</el-form-item>
+					</el-form>
+				</el-main>
+			</el-aside>
+		</el-container>
+
+
 	</el-container>
 </template>
 <script>
     import { mapState } from 'vuex'
+    import ElMain from "element-ui/packages/main/src/main";
     export default {
-		data() {
+        components: {ElMain},
+        data() {
 			return {
 
 			}
