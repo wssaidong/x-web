7d2a0dfdbd93d55cee0ff1250e13e22dee0bb017
caishaodong <sdcai@fengchaoli.com>
2018-08-24 16:44:43 +0800

    deploy

--- src/pages/flyfile/index.vue
+++ src/pages/flyfile/index.vue
@@ -9,25 +9,44 @@
 			<vHeader></vHeader>
 		</el-header>
 		<el-main class="body">
-			<el-form :model="form" :rules="rules" ref="form">
-				<el-form-item prop="fileCode">
-					<el-input
-							placeholder="文件码"
-							prefix-icon="el-icon-search"
-							v-model="form.code">
-					</el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="onSubmit('form')" icon="el-icon-search">搜索</el-button>
-					<el-button @click="resetForm('form')">重置</el-button>
-					<div v-if="flyfile" v-for="item in flyfile.fileList">
-						<a  v-bind:href="item.url" download="file" style="margin-left: 10px">
-							<el-button type="info" icon="el-icon-message" >{{item.fileName}}</el-button>
-						</a>
-					</div>
+			<div style="width: 50%;margin: 15% auto">
+				<el-form :model="form" :rules="rules" ref="form">
+					<el-form-item prop="code">
+						<el-input
+								placeholder="文件码"
+								v-model="form.code">
+							<el-button slot="append" icon="el-icon-search" @click="onSubmit('form')"></el-button>
+						</el-input>
+					</el-form-item>
+				</el-form>
+				<div>
+					<el-table
+							:data="flyfile.fileList"
+							style="width: 100%">
+						<el-table-column
+								prop="fileName"
+								label="文件名"
+								width="180"
+						>
+						</el-table-column>
+						<el-table-column
+								prop="url"
+								label="地址">
+						</el-table-column>
 
-				</el-form-item>
-			</el-form>
+						<el-table-column label="操作" width="100">
+							<template slot-scope="scope">
+								<el-button size="mini">
+									<a :href="scope.row.url" target= _blank>下载</a>
+								</el-button>
+
+							</template>
+						</el-table-column>
+					</el-table>
+
+				</div>
+			</div>
+
 		</el-main>
 	</el-container>
 </template>
@@ -42,7 +61,7 @@
                 rules: {
                     code: [
                         { required: true, message: '请输入文件码', trigger: 'blur' },
-                        { min: 2, max: 6, message: '长度在 2 到 6 个字符', trigger: 'blur' }
+                        //{ min: 2, max: 6, message: '长度在 2 到 6 个字符', trigger: 'blur' }
                     ],
                 }
             }
@@ -51,24 +70,15 @@
         computed: mapState({ flyfile: state => state.flyfile.flyfile }),
         methods: {
             onSubmit(formName) {
-
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
-                        console.log('submit!');
-
                         this.$store.dispatch('GET_FILE',this.form)
 					}else{
                         return false
 					}
 				})
 
-            },
-            resetForm(formName) {
-                this.$store.dispatch('CLEAR_FILE')
-                this.$refs[formName].resetFields();
-
-            },
-
+            }
         }
     }
 </script>
--- src/pages/flyfile/uploadFile.vue
+++ src/pages/flyfile/uploadFile.vue
@@ -1,5 +1,7 @@
 <style>
-
+    .el-upload-list__item-name{
+        text-align: left;
+    }
 </style>
 <template>
     <el-container>
@@ -7,24 +9,28 @@
             <vHeader></vHeader>
         </el-header>
         <el-main class="body">
-            <el-upload
-                    ref="upload"
-                    :limit="1"
-                    :on-change="handleChange"
-                    drag
-                    action="http://api.laystall.top/x/api/file"
+            <div style="width: 50%;margin: 5% auto;text-align: center">
+                <el-upload
+                        ref="upload"
+                        :limit="5"
+                        :on-change="handleChange"
+                        drag
+                        action="http://api.laystall.top/x/api/file?dir=flyFile/tmp"
+                        multiple>
+                    <i class="el-icon-upload"></i>
+                    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                    <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                </el-upload>
 
-                    multiple>
-                <i class="el-icon-upload"></i>
-                <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-                <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-            </el-upload>
+                <div style="margin-top: 20px">
+                    <el-button size="small" type="success" @click="submitUpload">生成文件码</el-button>
+                </div>
 
-            <el-button size="small" type="success" @click="submitUpload">生成文件码</el-button>
-
-            <div v-if="fileCode" style="margin-left: 10px">
-                {{fileCode}}
+                <div v-if="fileCode" style="margin-top: 10px">
+                    <el-tag type="success">{{fileCode}}</el-tag>
+                </div>
             </div>
+
         </el-main>
     </el-container>
 </template>
@@ -35,19 +41,28 @@
             return {
                 fileList: [],
                 fileCode: null,
+                fileChange:false,
             };
         },
 
         methods: {
             handleChange(file, fileList) {
                 this.fileList = fileList;
+                this.fileChange = true;
             },
             submitUpload() {
+                if( this.fileList.length == 0){
+                    return ;
+                }
+                if(!this.fileChange){
+                    return ;
+                }
                 var data = this.fileList.map(item => {
                     return {fileName:item.response.fileName,url:item.response.url};
                 })
                 flyFileUpload({fileList:JSON.stringify(data)}).then(response => {
                     this.fileCode = response.data.code
+                    this.fileChange = false;
                 })
             },
         }
